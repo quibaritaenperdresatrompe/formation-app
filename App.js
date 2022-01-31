c1c83f995d6cdfd09966ca9e8b74cc3b4d23f3bc
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2022-01-31 16:34:16 +0100

    Commit

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  debugger;
   console.log("App is running :)");
 
   return (
